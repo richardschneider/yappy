376f8c01a5d5c487650a26544663846b1f70ba0b
Richard Schneider <makaretu@gmail.com>
2015-10-28 04:10:15 +0000

    #26 verify entity link syntax

---
+++ server/model/link.js
@@ -0,0 +1,4 @@
+'use strict';
+
+// link form is /api/<type>/<id>
+module.exports = /^\/api\/\w+\/\w+$/;
---
+++ test/link.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var should = require('should');
+var schema = require('js-schema');
+var link = require('../server/model/link');
+
+
+describe('Link data type', function () {
+    let image = schema({
+       href: link
+    });    
+
+    it('starts with "/api/<type>/<id>"', function (done) {
+        image({ href: "/foo/bear/1" }).should.be.false;
+        image({ href: "/api/bear/1" }).should.be.true;
+        done();
+    });
+   
+
+});
