af4bfa176a163da0337247ed16d065d0dbea55d1
Richard Schneider <makaretu@gmail.com>
2015-11-01 21:23:41 +0000

    Use the json:api server

--- server/index.js
+++ server/index.js
@@ -1,121 +1,158 @@
-'use strict';
+"use strict";
 
-var express = require('express');
-var bodyParser = require('body-parser');
-var mongoskin = require('mongoskin');
-var cache = require('cache-control');
-var morgan = require('morgan');
-var compression = require('compression')
-var model = require('./model');
+var jsonApi = require("jsonapi-server");
+var fs = require("fs");
+var path = require("path");
 
-var app = express();
-//app.use(morgan('dev')); // log requests to the console
-app.use(compression());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(cache({
-    '/api/media/*/content': 'max-stale=31536000', // media views are indempotent
-    '/**': false // Default is no caching
-}));
-app.set('json spaces', 2);
-
-app.get('/api', function(req, res) {
-    res.json({ message: 'Welcome to the e-commerce API!' });
+jsonApi.setConfig({
+  base: "api",
+  meta: {
+    copyright: "ecom by me"
+  }
 });
 
-app.use('/api', require('./media'));
+fs.readdirSync(path.join(__dirname, "/resource")).filter(function(filename) {
+  return /^[a-z].*\.js$/.test(filename);
+}).map(function(filename) {
+  return path.join(__dirname, "/resource/", filename);
+}).map(function(filename) {
+    console.log("loading " + filename);
+    return filename;
+}).forEach(require);
 
-app.param('collectionName', function(req, res, next, collectionName){
-    var dataModel = model[collectionName];
-    if (!dataModel)
-        return res.status(400).json({ message: 'Unknown model name.'});
-    
-    var readonlyMethods = [ 'GET', 'HEAD' ];
-    if (dataModel.readOnly && readonlyMethods.indexOf(req.method) < 0)
-        return res.status(405).json({ message: 'Model is read only'});
-
-    req.dataModel = dataModel;
-    if (collectionName == 'media')
-        req.collection = dataModel.db.collection('fs.files');
-    else
-        req.collection = dataModel.db.collection(collectionName);
-    
-    return next();
+jsonApi.onUncaughtException(function(request, error) {
+  var errorDetails = error.stack.split("\n");
+  console.error(JSON.stringify({
+    request: request,
+    error: errorDetails.shift(),
+    stack: errorDetails
+  }));
 });
 
-app.get('/api/:collectionName', function(req, res, next) {
-  req.collection.find({} ,{limit: 10, sort: {'_id': -1}}).toArray(function(e, results){
-    if (e) return next(e);
-    res.send(results).end();
-  });
-});
+jsonApi.start();
 
-app.post('/api/:collectionName', function(req, res, next) {
-    var now = new Date();
-    
-    if (!req.dataModel.validate(req, res))
-        return;
-    
-    req.body.modifiedOn = now.toISOString();
-    req.collection.insert(req.body, {}, function(e, results) {
-        if (e) return next(e);
-    
-        var id = results.insertedIds[0];
-        var url = '/api/' + req.params.collectionName + '/' + id;
-        res
-            .status(201)
-            .location(url)
-            .set('Last-Modified', now.toUTCString())
-            .send({ status: 'ok', self: url})
-            .end();
-      });
-});
+module.exports = jsonApi.express;
 
-app.get('/api/:collectionName/schema', function(req, res, next) {
-    var schema = req.dataModel.schema;
-    if (!schema)
-        return res.status(404).end();
+
+
+// 'use strict';
+
+// var express = require('express');
+// var bodyParser = require('body-parser');
+// var mongoskin = require('mongoskin');
+// var cache = require('cache-control');
+// var morgan = require('morgan');
+// var compression = require('compression');
+// var model = require('./model');
+
+// var app = express();
+// //app.use(morgan('dev')); // log requests to the console
+// app.use(compression());
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+// app.use(cache({
+//     '/api/media/*/content': 'max-stale=31536000', // media views are indempotent
+//     '/**': false // Default is no caching
+// }));
+// app.set('json spaces', 2);
+
+// app.get('/api', function(req, res) {
+//     res.json({ message: 'Welcome to the e-commerce API!' });
+// });
+
+// app.use('/api', require('./media'));
+
+// app.param('collectionName', function(req, res, next, collectionName){
+//     var dataModel = model[collectionName];
+//     if (!dataModel)
+//         return res.status(400).json({ message: 'Unknown model name.'});
+    
+//     var readonlyMethods = [ 'GET', 'HEAD' ];
+//     if (dataModel.readOnly && readonlyMethods.indexOf(req.method) < 0)
+//         return res.status(405).json({ message: 'Model is read only'});
+
+//     req.dataModel = dataModel;
+//     if (collectionName == 'media')
+//         req.collection = dataModel.db.collection('fs.files');
+//     else
+//         req.collection = dataModel.db.collection(collectionName);
+    
+//     return next();
+// });
+
+// app.get('/api/:collectionName', function(req, res, next) {
+//   req.collection.find({} ,{limit: 10, sort: {'_id': -1}}).toArray(function(e, results){
+//     if (e) return next(e);
+//     res.send(results).end();
+//   });
+// });
+
+// app.post('/api/:collectionName', function(req, res, next) {
+//     var now = new Date();
+    
+//     if (!req.dataModel.validate(req, res))
+//         return;
+    
+//     req.body.modifiedOn = now.toISOString();
+//     req.collection.insert(req.body, {}, function(e, results) {
+//         if (e) return next(e);
+    
+//         var id = results.insertedIds[0];
+//         var url = '/api/' + req.params.collectionName + '/' + id;
+//         res
+//             .status(201)
+//             .location(url)
+//             .set('Last-Modified', now.toUTCString())
+//             .send({ status: 'ok', self: url})
+//             .end();
+//       });
+// });
+
+// app.get('/api/:collectionName/schema', function(req, res, next) {
+//     var schema = req.dataModel.schema;
+//     if (!schema)
+//         return res.status(404).end();
         
-    res.send(schema.toJSON()).end();
-});
+//     res.send(schema.toJSON()).end();
+// });
 
-app.get('/api/:collectionName/:id', function(req, res, next) {
-    req.collection.findById(req.params.id, function(e, entity) {
-        if (e) return next(e);
+// app.get('/api/:collectionName/:id', function(req, res, next) {
+//     req.collection.findById(req.params.id, function(e, entity) {
+//         if (e) return next(e);
         
-        res
-            .set('Last-Modified', new Date(entity.modifiedOn).toUTCString())
-            .send(entity)
-            .end();
-    });
-});
+//         res
+//             .set('Last-Modified', new Date(entity.modifiedOn).toUTCString())
+//             .send(entity)
+//             .end();
+//     });
+// });
 
-app.put('/api/:collectionName/:id', function(req, res, next) {
-    var now = new Date();
+// app.put('/api/:collectionName/:id', function(req, res, next) {
+//     var now = new Date();
 
-    if (!req.dataModel.validate(req, res))
-        return;
+//     if (!req.dataModel.validate(req, res))
+//         return;
 
-    req.body.modifiedOn = now.toISOString();
-    req.collection.updateById(req.params.id, {$set: req.body}, {safe: true, multi: false}, function(e, result){
-        if (e) return next(e);
+//     req.body.modifiedOn = now.toISOString();
+//     req.collection.updateById(req.params.id, {$set: req.body}, {safe: true, multi: false}, function(e, result){
+//         if (e) return next(e);
 
-        res
-            .set('Last-Modified', now.toUTCString())
-            .send((result === 1) ? {status:'ok'} : {status: 'error'})
-            .end();
-    });
-});
+//         res
+//             .set('Last-Modified', now.toUTCString())
+//             .send((result === 1) ? {status:'ok'} : {status: 'error'})
+//             .end();
+//     });
+// });
 
-app.delete('/api/:collectionName/:id', function(req, res, next) {
-    req.collection.removeById(req.params.id, function (e, result) {
-    if (e) return next(e);
+// app.delete('/api/:collectionName/:id', function(req, res, next) {
+//     req.collection.removeById(req.params.id, function (e, result) {
+//     if (e) return next(e);
     
-    res
-        .status(204)
-        .end();
-  });
-});
+//     res
+//         .status(204)
+//         .end();
+//   });
+// });
 
 
-module.exports = app;
+// module.exports = app;
