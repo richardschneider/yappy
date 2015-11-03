19e621f17f2088f4e5651a7d1bce687db4a44ba0
Richard Schneider <makaretu@gmail.com>
2015-11-03 08:37:19 +0000

    Send audit infomation to 'sally.log' #38

--- server/sally.js
+++ server/sally.js
@@ -3,11 +3,13 @@
 var crypto = require('crypto');
 var onFinished = require('on-finished');
 var os = require('os');
+const fs = require('fs');
 const util = require('util');
 const EventEmitter = require('events').EventEmitter;
 
 var config = {};
 var noDigest;
+var previousDigest;
 
 function Sally(opts) {
     // Initialize necessary properties from `EventEmitter` in this instance
@@ -19,7 +21,8 @@ function Sally(opts) {
     config.auditMethods = opts.auditMethods || ['POST', 'PUT', 'DELETE', 'PATCH'];
     config.user = opts.user || function() { return 'anonymous'; };
     config.hostname = opts.hostname || os.hostname();
-
+    config.filename = opts.filename || 'sally.log';
+    
     noDigest = crypto.createHmac(config.hash, config.secret)
         .update('')
         .digest('base64');
@@ -30,6 +33,11 @@ util.inherits(Sally, EventEmitter);
 
 var self = module.exports = new Sally();
 
+/**
+ * Adds the sally logger to the express pipeline.
+ * 
+ * It generates audit record for every request that creates/modifies a resource.
+ */
 self.logger = function (req, res, next){
     var now = new Date();
 
@@ -53,15 +61,17 @@ self.logger = function (req, res, next){
             what: (req.method == 'POST') ? res.header['Location'] : req.url,
             status: res.statusCode,
         };
-        var digest = self.sign(audit);
-        self.emit("log", audit, digest);
+        self.log(audit);
     }
     
     onFinished(res, logRequest);
     next();
 };
 
-self.sign = function (audit, prevDigest) {
+/**
+ * Internal method to sign an audit entry.
+ */
+ self.sign = function (audit, prevDigest) {
     if (typeof audit === 'object')
         audit = JSON.stringify(audit);
     if (!prevDigest)
@@ -73,7 +83,28 @@ self.sign = function (audit, prevDigest) {
         .digest('base64');
 };
 
+/**
+ * Verifies that the audit information has not been tampered with.
+ */
 self.verify = function (audit, digest, prevDigest) {
     return digest == self.sign(audit, prevDigest);
+};
+
+/**
+ * Adds a entry into the audit.
+ * 
+ * Signs the audit information and then emits the 'log' event.  Audit logs
+ * can then append the audit information to their log.
+ */
+self.log = function(audit) {
+    previousDigest = self.sign(audit, previousDigest);
+    self.emit("log", audit, previousDigest);
+    var entry = JSON.stringify({
+        audit: audit,
+        digest: previousDigest
+    });
+    fs.appendFile(config.filename, entry + '\n', function (err) {
+        if (err) throw err;
+    });
 };
 
