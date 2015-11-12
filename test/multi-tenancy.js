97aa85341566bd4f4c415662e3baae860a5e4e1d
Richard Schneider <makaretu@gmail.com>
2015-11-12 21:38:38 +0000

    Some tests for #43

---
+++ test/multi-tenancy.js
@@ -0,0 +1,44 @@
+'use strict';
+
+var request = require("supertest-as-promised");
+var server = require('../lib/server');
+
+describe('Multi-tenancy', function () {
+
+    var test1Url;
+    before(function (done) {
+        request(server)
+            .post('/api/tenant')
+            .send({
+                name: [{tag: 'en', text: 'me'}],
+                domain: 'test-1'
+            })
+            .expect(201)
+            .expect(res => { test1Url = res.header['location']; })
+            .end(done);
+    });
+    
+    after(function (done) {
+        request(server)
+            .delete(test1Url)
+            .expect(204)
+            .end(done);
+    });
+
+    it('should return 400 Bad Request when tenant is unknown', function (done) {
+        request(server)
+            .get('/api/bear')
+            .set('host', 'unknown.ecom.io')
+            .expect(400, { message: 'Check the host name, especially unknown' }, done);
+    });
+    
+    it('should allow access when tenant is known', function (done) {
+        request(server)
+            .get('/api/bear')
+            .set('host', 'test-1.ecom.io')
+            .expect(200)
+            .end(done);
+    });
+
+    
+});
