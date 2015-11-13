e29a784f2fd342781b80ea95f6c666d7cd7ffcb3
Richard Schneider <makaretu@gmail.com>
2015-11-13 23:56:50 +0000

    #45 create an update record if required

---
+++ test/upgrade.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var request = require("supertest-as-promised");
+var should = require('should');
+var server = require('../lib/server');
+
+
+describe('Upgrade', function () {
+
+    it('should always have a history', function (done) {
+        request(server)
+            .get('/api/config/upgrade')
+            .expect(200)
+            .end(done);            
+    });
+   
+
+});
