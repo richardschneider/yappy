8fb1b11b6cc08d07cd6fb67642b06a5ed2d6874f
Richard Schneider <makaretu@gmail.com>
2015-11-15 21:31:28 +0000

    more translation tests, #60

--- test/l10n.js
+++ test/l10n.js
@@ -19,11 +19,34 @@ describe('l10n', () => {
             .finally(() => done());
         });
         
-        it('should support french translations', done => {
+        it('should support French (fr) translations', done => {
             l10n.translate(null, 'its time for a beer', 'fr')
             .then(t => t.translation.should.equal('son temps pour une bière'))
             .finally(() => done());
         });
 
+        it('should support Chineese (zh) translations', done => {
+            l10n.translate(null, 'its time for a beer', 'zh')
+            .then(t => t.translation.should.equal('一杯啤酒的时候了'))
+            .finally(() => done());
+        });
+
+        it('should support Mainland China (zh-CN) translations', done => {
+            l10n.translate(null, 'its time for a beer', 'zh-CN')
+            .then(t => t.translation.should.equal('一杯啤酒的时候了'))
+            .finally(() => done());
+        });
+
+        it('should support Hong Kong (zh-HK) translations', done => {
+            l10n.translate(null, 'its time for a beer', 'zh-HK')
+            .then(t => t.translation.should.equal('一杯啤酒的时候了'))
+            .finally(() => done());
+        });
+
+        it('should support Taiwaneese (zh-TW) translations', done => {
+            l10n.translate(null, 'its time for a beer', 'zh-TW')
+            .then(t => t.translation.should.equal('一杯啤酒的時候了'))
+            .finally(() => done());
+        });
    }) 
 });
