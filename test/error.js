693f00840000e827b6715a19de78b31ef1508df0
Richard Schneider <makaretu@gmail.com>
2015-11-16 22:30:08 +0000

    Need a fallback language until the tenant is found, #64

--- test/error.js
+++ test/error.js
@@ -7,14 +7,37 @@ var server = require('../lib/server');
 describe('Error', () => {
 
     var err;
+    var domainUrl;
     before(done => {
         request(server)
-        .get('/api/bear/unknown')
-        .set('Accept-Language', 'fr')
-        .expect(404)
-        .expect(r => err = r)
-        .end(done);
+            .get('/api/bear/unknown')
+            .set('Accept-Language', 'fr')
+            .expect(404)
+            .expect(r => { err = r; })
+        .then(() => {
+        request(server)
+            .post('/api/tenant')
+            .send({
+                name: [{tag: 'en', text: 'moi'}],
+                languages: ['fr'],
+                domain: 'fr-only'
+            })
+            .expect(201)
+            .expect(res => { 
+                domainUrl = res.header['location']; 
+                console.log(domainUrl)
+            })
+            .end(done);
+        });
     }); 
+    
+    after(done => {
+        request(server)
+            .delete(domainUrl)
+            .set('host', 'fr-only.ecom.io')
+            .expect(204)
+            .end(done);
+    });
 
     it('should have a message', done => {
         err.body.should.have.property('message');
@@ -33,34 +56,15 @@ describe('Error', () => {
     });
     
     it('should only use the language(s) of the tentant', done => {
-        var domainUrl;
         request(server)
-            .post('/api/tenant')
-            .send({
-                name: [{tag: 'en', text: 'me'}],
-                languages: ['fr'],
-                domain: 'fr-only'
+            .get('/api/bear/unknown')
+            .set('host', 'fr-only.ecom.io')
+            .expect(404)
+            .expect(err => {
+                err.headers.should.have.property('content-language', 'fr');
+                err.body.message.should.endWith('trouvé');
             })
-            .expect(201)
-            .expect(res => { domainUrl = res.header['location']; })
-            .then(() => {
-                return request(server)
-                .get('/api/bear/unknown')
-                .set('host', 'fr-only.ecom.io')
-                .expect(404)
-                .expect(err => {
-                    err.headers.should.have.property('content-language', 'fr');
-                    err.body.message.should.endWith('trouvé');
-                    done();
-                });
-            })
-            .catch(e => done(e))
-            .finally(() => {
-                request(server)
-                .delete(domainUrl)
-                .set('host', 'fr-only.ecom.io')
-                .expect(204)
-            });
+            .end(done);
     });
 
 
