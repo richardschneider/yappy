ab2eb6330dc6b1f205aeac1ac2b58ec9ca4905d2
Richard Schneider <makaretu@gmail.com>
2015-11-16 11:07:22 +0000

    best language/local is now determined from tenant laguages, #64

--- test/error.js
+++ test/error.js
@@ -26,10 +26,42 @@ describe('Error', () => {
         done();
     });
 
-    it('should be translated', done => {
+    it('should be translated per Accept-Language', done => {
         err.headers.should.have.property('content-language', 'fr');
         err.body.message.should.endWith('trouvé');
         done();
     });
+    
+    it('should only use the language(s) of the tentant', done => {
+        var domainUrl;
+        request(server)
+            .post('/api/tenant')
+            .send({
+                name: [{tag: 'en', text: 'me'}],
+                languages: ['fr'],
+                domain: 'fr-only'
+            })
+            .expect(201)
+            .expect(res => { domainUrl = res.header['location']; })
+            .then(() => {
+                return request(server)
+                .get('/api/bear/unknown')
+                .set('host', 'fr-only.ecom.io')
+                .expect(404)
+                .expect(err => {
+                    err.headers.should.have.property('content-language', 'fr');
+                    err.body.message.should.endWith('trouvé');
+                    done();
+                });
+            })
+            .catch(e => done(e))
+            .finally(() => {
+                request(server)
+                .delete(domainUrl)
+                .set('host', 'fr-only.ecom.io')
+                .expect(204)
+            });
+    });
+
 
 });
