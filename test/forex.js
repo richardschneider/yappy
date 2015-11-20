0f55708453b49da2d36e9c6c10439d9c003d91f3
Richard Schneider <makaretu@gmail.com>
2015-11-20 01:51:24 +0000

    added /forex route for #67

---
+++ test/forex.js
@@ -0,0 +1,56 @@
+'use strict';
+
+var should = require('should');
+var request = require("supertest-as-promised");
+var server = require('../lib/server');
+var iso = require('../lib/model/iso');
+
+describe ('Forex', () => {
+
+    it('should default the base currency', done => {
+        request(server)
+            .get('/forex')
+            .expect(200)
+            .expect(res => {
+                let rates = res.body;
+                rates.should.have.property('base');
+                rates.should.have.property('date')
+                    .and.match(iso.date);
+                rates.should.have.property('rates')
+                    .and.have.property('NZD');
+            })
+            .end(done);
+    });
+
+    it('should return CNY exchange rates', done => {
+        request(server)
+            .get('/forex/CNY')
+            .expect(200)
+            .expect(res => {
+                let rates = res.body;
+                rates.should.have.property('base', 'CNY');
+                rates.should.have.property('date')
+                    .and.match(iso.date);
+                rates.should.have.property('rates')
+                    .and.have.property('NZD');
+            })
+            .end(done);
+    });
+
+    it('should return error on valid but unknown currency', done => {
+        request(server)
+            .get('/forex/AAA')
+            .expect(res => {
+                res.status.should.be.above(399);
+            })
+            .end(done);
+    });
+
+    it('should return error on invalid currency', done => {
+        request(server)
+            .get('/forex/invalid-currency')
+            .expect(400)
+            .end(done);
+    });
+    
+});
