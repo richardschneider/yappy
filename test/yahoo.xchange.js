e4bcbeebb3fa0455ccd072e0f2da98e0305abe99
Richard Schneider <makaretu@gmail.com>
2015-11-21 07:28:39 +0000

    yahoo.xchange service adaptor, closes #78

---
+++ test/yahoo.xchange.js
@@ -0,0 +1,47 @@
+'use strict';
+
+var should = require('should');
+var request = require("supertest-as-promised");
+var server = require('../lib/server');
+var iso = require('../lib/model/iso')
+var fx = require('../lib/service/yahoo.xchange');
+
+describe ('Yahoo XChange forex rates', () => {
+
+    it('should be a published service', done => {
+        request(server)
+            .get('/api/tenant')
+            .expect(200)
+            .expect(res => {
+                let tenant = res.body[0];
+                let x = tenant.services.find(e => e.moduleName == 'yahoo.xchange');
+                should.exist(x);
+            })
+            .end(done);
+    });
+    
+    it('should error when no currencies of interest', done => {
+        fx('NZD', [], fx.details.options)
+            .then(() => done('SHOULD NOT HAPPNEN'))
+            .catch(e => done());
+    });
+
+    it('should only return currencies of interest', done => {
+        fx('NZD', ['AUD', 'CNY'], fx.details.options)
+            .then(rates => {
+                rates.should.have.property('base', 'NZD');
+                rates.should.have.property('date')
+                    .and.match(iso.dateTime);
+                rates.should.have.property('rates');
+                Object.keys(rates.rates).should.have.lengthOf(2);
+                rates.rates.should.have.property('AUD')
+                    .and.be.a.Number;
+                rates.rates.should.have.property('CNY')
+                    .and.be.a.Number;
+                done();
+            })
+            .catch(done);
+    });
+
+    
+});
