867c9ff52daa53402d82635f3653dd1badd84cc4
Richard Schneider <makaretu@gmail.com>
2015-11-30 10:13:57 +0000

    implement authz using express-authorization #83

---
+++ test/authorisation.js
@@ -0,0 +1,34 @@
+'use strict';
+
+let authz = require("../lib/server/authorisation");
+
+describe('Authorisation', function () {
+
+    it('should allow read-only users to resources', () => {
+        let user = { permissions: ['api:*:view', 'api:*:find'] };
+        authz.isPermitted(user, 'api:product:view').should.be.true;
+        authz.isPermitted(user, 'api:product:view:123').should.be.true;
+        authz.isPermitted(user, 'api:product:find').should.be.true;
+        authz.isPermitted(user, 'api:product:create').should.be.false;
+        authz.isPermitted(user, 'api:product:change:123').should.be.false;
+        authz.isPermitted(user, 'api:product:delete:123').should.be.false;
+    });
+
+    it('should allow a user to manage only one resource type', () => {
+        let user = { permissions: ['api:product:*'] };
+        authz.isPermitted(user, 'api:product:view').should.be.true;
+        authz.isPermitted(user, 'api:product:view:123').should.be.true;
+        authz.isPermitted(user, 'api:product:find').should.be.true;
+        authz.isPermitted(user, 'api:product:create').should.be.true;
+        authz.isPermitted(user, 'api:product:change:123').should.be.true;
+        authz.isPermitted(user, 'api:product:delete:123').should.be.true;
+
+        authz.isPermitted(user, 'api:user:view').should.be.false;
+        authz.isPermitted(user, 'api:user:view:123').should.be.false;
+        authz.isPermitted(user, 'api:user:find').should.be.false;
+        authz.isPermitted(user, 'api:user:create').should.be.false;
+        authz.isPermitted(user, 'api:user:change:123').should.be.false;
+        authz.isPermitted(user, 'api:user:delete:123').should.be.false;
+    });
+
+});
