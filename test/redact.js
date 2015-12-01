e4aff4fc1c727bdde5119618fe43e22382aac159
Richard Schneider <makaretu@gmail.com>
2015-12-01 10:18:12 +0000

    redact should work with the results of a search #107

--- test/redact.js
+++ test/redact.js
@@ -4,15 +4,12 @@ require('should');
 var redact = require('../lib/server/redact');
 
 describe ('Redact', () => {
-    
-    
-
 
     let req = {
             method: 'PUT'
         },
         res = {};
-    
+
     it('should not show the plain text API key when viewing the tenant', () => {
         let plain = {
             services: [
@@ -35,7 +32,25 @@ describe ('Redact', () => {
             ]
         };
         let redacted = redact.document(plain, req, res);
-        redacted.should.have.property(redact.metadataName);       
+        redacted.should.have.property(redact.metadataName);
+    });
+
+    it('should work with the results of a search', () => {
+        let searchResults = {
+            links: { },
+            data: [
+                { name: 'a', apikey: 'a'},
+                { name: 'b' },
+                { name: 'c', apikey: 'c'}
+            ]
+        };
+        let redacted = redact.document(searchResults, req, res);
+        redacted.data[0].apikey.should.equal(redact.mask);
+        redacted.data[2].apikey.should.equal(redact.mask);
+        redacted.data[0].should.have.property(redact.metadataName);
+        redacted.data[1].should.not.have.property(redact.metadataName);
+        redacted.data[2].should.have.property(redact.metadataName);
+        redacted.should.not.have.property(redact.metadataName);
     });
 
     it('should work with array of documents', () => {
@@ -59,10 +74,10 @@ describe ('Redact', () => {
         redacted[0].services[2].apikey.should.equal(redact.mask);
         redacted[1].services[0].apikey.should.equal(redact.mask);
         redacted[1].services[2].apikey.should.equal(redact.mask);
-        redacted[0].should.have.property(redact.metadataName);       
-        redacted[1].should.have.property(redact.metadataName);       
-        redacted[2].should.not.have.property(redact.metadataName);       
-        redacted.should.not.have.property(redact.metadataName);       
+        redacted[0].should.have.property(redact.metadataName);
+        redacted[1].should.have.property(redact.metadataName);
+        redacted[2].should.not.have.property(redact.metadataName);
+        redacted.should.not.have.property(redact.metadataName);
     });
 
     it('should not add metadata when no changes are made', () => {
@@ -74,9 +89,9 @@ describe ('Redact', () => {
             ]
         };
         let redacted = redact.document(plain_noapikey, req, res);
-        redacted.should.not.have.property(redact.metadataName);       
+        redacted.should.not.have.property(redact.metadataName);
     });
-    
+
     it('should not allow update of redacted document', () => {
         let plain = {
             services: [
@@ -87,6 +102,18 @@ describe ('Redact', () => {
         };
         let redacted = redact.document(plain, req, res);
         redact.allowUpdate(redacted, req, res).should.equal(false);
+    });
+
+    it('should allow patching of redacted document', () => {
+        let plain = {
+            services: [
+                { name: 'a', apikey: 'a'},
+                { name: 'b' },
+                { name: 'c', apikey: 'c'}
+            ]
+        };
+        let redacted = redact.document(plain, req, res);
+        redact.allowUpdate(redacted, { method: 'PATCH' }, res).should.equal(true);
     });
 
     it('should allow update of non-redacted document', () => {
@@ -100,5 +127,5 @@ describe ('Redact', () => {
         let redacted = redact.document(plain_noapikey, req, res);
         redact.allowUpdate(redacted, req, res).should.equal(true);
     });
-    
+
 });
