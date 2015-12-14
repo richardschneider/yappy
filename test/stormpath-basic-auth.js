29e965d0fee7a85f2db626c4c38020e064629dd0
Richard Schneider <makaretu@gmail.com>
2015-12-14 01:04:07 +0000

    basic auth from stormpath, closes #131

--- test/stormpath-basic-auth.js
+++ test/stormpath-basic-auth.js
@@ -6,6 +6,48 @@ let request = require("./my-supertest"),
 
 describe('Stormpath Basic authentication', function () {
 
+    let tenant;
+    let service_on = [ { "op": "replace", "path": "/service/stormpath-basic-auth/enabled", "value": true } ];
+    let service_off = [ { "op": "replace", "path": "/service/stormpath-basic-auth/enabled", "value": false } ];
+
+    // enable the service
+    before(done => {
+        request(server)
+            .get('/api/tenant')
+            .expect(200)
+            .then(res => {
+                tenant = res.body.data[0]._metadata.self;
+                return request(server)
+                    .patch(tenant)
+                    .set('content-type', 'application/json-patch+json')
+                    .send(JSON.stringify(service_on))
+                    .expect(204)
+                    .then(() => done())
+                    .catch(done);
+            });
+    });
+
+    after(done => {
+        return request(server)
+            .patch(tenant)
+            .set('content-type', 'application/json-patch+json')
+            .send(JSON.stringify(service_off))
+            .expect(204)
+            .then(() => done())
+            .catch(done);
+    });
+
+    it('should be a published service', done => {
+        request(server)
+            .get('/api/tenant')
+            .expect(200)
+            .expect(res => {
+                let tenant = res.body.data[0];
+                tenant.service.should.have.property('stormpath-basic-auth');
+            })
+            .end(done);
+    });
+
     it('should allow Basic auth', function (done) {
         request(server)
             .get('/api/whoami')
